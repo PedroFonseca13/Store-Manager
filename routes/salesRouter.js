a00570434ae0694340e0eda617a0078e62860e24
Pedro Fonseca <pedrinhopedro01@gmail.com>
2022-06-30 17:04:06 -0300

    :sparkles:  added routes getAll

--- routes/salesRouter.js
+++ routes/salesRouter.js
@@ -1,3 +1,6 @@
 const router = require('express').Router();
+const salesController = require('../controllers/salesController');
+
+router.get('/', salesController.getAll());
 
 module.exports = router;
