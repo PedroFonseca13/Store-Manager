a26fc84cc62ff0fb7dd4aa1566f6815b7226346d
Pedro Fonseca <pedrinhopedro01@gmail.com>
2022-06-30 17:47:19 -0300

    :bug: (salesRouter.js) fixed error in sales route

--- routes/salesRouter.js
+++ routes/salesRouter.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const salesController = require('../controllers/salesController');
 
-router.get('/', salesController.getAll());
+router.get('/sales', salesController.getAll);
 
 module.exports = router;
