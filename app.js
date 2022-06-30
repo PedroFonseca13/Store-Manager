4f3f22b212895cbc2f72ef99f4ed08ed2c65ee29
Pedro Fonseca <pedrinhopedro01@gmail.com>
2022-06-30 14:37:29 -0300

    feat: created and implemented product and sales routes

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const productsRouter = require('./routes/productsRouter');
+const salesRouter = require('./routes/salesRouter');
 
 const app = express();
+
+app.use(express.json());
+
+app.use(productsRouter);
+app.use(salesRouter);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
+app.use((err, _req, res, _next) => {
+  if (err.status) return res.status(err.status).json({ message: err.message });
+  return res.status(500).json({ message: err.message });
+});
+
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação 
+// você deve usar o arquivo index.js para executar sua aplicação
 module.exports = app;
