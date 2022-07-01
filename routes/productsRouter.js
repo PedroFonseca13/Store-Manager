ffdcf54b922d657ed0e874b2177e4d8754dec7ac
Pedro Fonseca <pedrinhopedro01@gmail.com>
2022-07-01 15:19:35 -0300

    :sparkles: (productsRouter.js) added router products/:id

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const productsController = require('../controllers/productsController');
 
 router.get('/products', productsController.getAll);
+router.get('/products/:id', productsController.findById);
 
 module.exports = router;
