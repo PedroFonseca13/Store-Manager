88187b019ab04b3f5a048e59d663bdab341e1c5e
Pedro Fonseca <pedrinhopedro01@gmail.com>
2022-07-01 15:16:02 -0300

    :sparkles: (productsController.js) added findByID/productsController

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -6,6 +6,19 @@ const getAll = async (_req, res) => {
   return res.status(200).json(products);
 };
 
+const findById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await productsService.findById(id);
+
+    return res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(404).json({ message: error.message });
+  }
+};
+
 module.exports = {
   getAll,
+  findById,
 };
