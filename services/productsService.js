73de17e887ab93e5b0595a19f1050545d2cb5985
Pedro Fonseca <pedrinhopedro01@gmail.com>
2022-07-01 15:18:33 -0300

    :sparkles: (productsService.js) added findByID/productsService

--- services/productsService.js
+++ services/productsService.js
@@ -1,11 +1,22 @@
 const productsModel = require('../models/productsModel');
 
+const errorHandler = (status, message) => ({ status, message });
+
 const getAll = async () => {
   const products = await productsModel.getAll();
 
   return products;
 };
 
+const findById = async (id) => {
+  const [product] = await productsModel.findById(id);
+
+  if (!product) throw errorHandler(404, 'Product not found');
+
+  return product;
+};
+
 module.exports = {
   getAll,
+  findById,
 };
