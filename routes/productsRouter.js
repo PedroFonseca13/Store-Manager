d3198b932642e1ace6d9e587c8ba5c2b6612b794
Pedro Fonseca <pedrinhopedro01@gmail.com>
2022-07-01 16:08:17 -0300

    :sparkles:  req04 implemented

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const productsController = require('../controllers/productsController');
+const productsMiddleware = require('../middleware/productsMiddleware');
 
 router.get('/products', productsController.getAll);
 router.get('/products/:id', productsController.findById);
-router.post('/products', productsController.registerProduct);
+router.post('/products', productsMiddleware.validateName, productsController.registerProduct);
 
 module.exports = router;
