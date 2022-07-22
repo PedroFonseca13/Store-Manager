8ada44ab46d4e9a6a585917c084b34df3917ff6d
Pedro Fonseca <pedrinhopedro01@gmail.com>
2022-07-22 16:54:58 -0300

    refatorando salesController. Te odeio back end

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -1,9 +1,14 @@
 const salesService = require('../services/salesService');
+const handleError = require('../utils/handleError');
 
 const getAll = async (_req, res) => {
-  const sales = await salesService.getAll();
+  try {
+    const products = await salesService.getAll();
 
-  return res.status(200).json(sales);
+    return res.status(200).json(products);
+  } catch (error) {
+    handleError(error, _req, res);
+  }
 };
 
 const findById = async (req, res) => {
@@ -15,4 +20,18 @@ const findById = async (req, res) => {
   return res.status(200).json(sale);
 };
 
-module.exports = { getAll, findById };
+const addSale = async (req, res) => {
+  const sale = req.body;
+  const saleData = await salesService.newSales(sale);
+
+  return res.status(201).json(saleData);
+};
+
+const deleteSale = async (req, res) => {
+  const { id } = req.params;
+
+  await salesService.deleteSale(id);
+  res.status(204).end();
+};
+
+module.exports = { getAll, findById, addSale, deleteSale };
