ca3009f83e33c1bb1c01df1cdae9251503f08580
Pedro Fonseca <pedrinhopedro01@gmail.com>
2022-08-01 15:45:42 -0300

    test: implementado testes do productsModel

--- tests/unit/models/productsModel.test.js
+++ tests/unit/models/productsModel.test.js
@@ -6,86 +6,109 @@ const productsModel = require('../../../models/productsModel');
 
 describe('Testes de productsModel', () => {
 
-  describe('Verificação do getAll', () => {
-    const produtcs = [
-      {
-        "id": 1,
-        "name": "Martelo de Thor",
-      },
-      {
-        "id": 2,
-        "name": "Traje de encolhimento",
-      }
-    ];
+  describe('Verificação da função getAll', () => {
 
     before(async () => {
-      sinon.stub(connection, 'execute').returns([produtcs]);
+      const result = [[{ id: 1 }]];
+      sinon.stub(connection, 'execute').resolves(result);
     });
 
     after(async () => {
       connection.execute.restore();
     })
 
-    it('retorna um array de produtos', async () => {
+    it('returns an array', async () => {
       const response = await productsModel.getAll();
-      expect(response).to.be.a('array');
-    })
-  });
-
-  describe('Verificação do findById', () => {
-
-    const product = {
-      id: '1',
-      name: 'Martelo de Thor'
-    }
-
-    before(async () => {
-      sinon.stub(connection, 'execute').returns([[product]])
-    })
-
-    after(async () => {
-      connection.execute.restore();
-    })
-
-    it('Retorna um objeto que representa o produto', async () => {
-
-      const response = await productsModel.findById(1);
-      expect(response).to.be.a('object')
-    })
-
-    it('deve disparar um erro caso o connection.query dispare um erro', () => {
-      sinon.stub(connection, 'query').rejects();
-      chai.expect(productsModel.findById(0)).to.eventually.be.rejected;
+      expect(response).to.be.an('array');
     });
 
-    it('deve retornar nada caso o connection.query retorne uma lista vazia', () => {
-      sinon.stub(connection, 'query').resolves([[]]);
-      chai.expect(productsModel.findById(0)).to.eventually.be.undefined;
+    it('inside the array exists at least one object', async () => {
+      const response = await productsModel.getAll();
+      expect(response[0]).to.be.an('object');
     });
 
-    it('deve retornar um objeto caso o connection.query retorne um item na lista', () => {
-
+    it('inside this object exists a property ID', async () => {
+      const response = await productsModel.getAll();
+      expect(response[0]).to.have.property('id');
     });
   });
 
-  describe('Verificação do registerProduct', () => {
-    const product = {
-      id: '4',
-      name: 'Martelo de Thor'
-    }
+  describe('Verificação da função findById', () => {
 
     before(async () => {
-      sinon.stub(connection, 'execute').returns([[product]])
+      const result = [[{ id: 1 }]];
+      sinon.stub(connection, 'execute').resolves(result);
     });
 
     after(async () => {
       connection.execute.restore();
-    })
+    });
 
-    it('Retorna um objeto que representa o produto', async () => {
-      const { name } = product
-      const response = await productsModel.registerProduct(name);
-      expect(response).to.be.a('object')
+    describe('When the request is successfully completed', () => {
+      it('returns an array', async () => {
+        const response = await productsModel.findById(1);
+        expect(response).to.be.an('array');
+      });
+
+      it('inside the array exists one object', async () => {
+        const response = await productsModel.findById(1);
+        expect(response[0]).to.be.an('object');
+        expect(response.length).to.be.equal(1);
+      });
+
+      it('inside this object exists a property ID', async () => {
+        const response = await productsModel.findById(1);
+        expect(response[0]).to.have.property('id');
+      });
+    });
+  });
+
+  describe('Verificação da função registerProduct', () => {
+    describe('When the POST is successfully completed', () => {
+      const MOCK_NAME = 'Steve Rogers'
+      const MOCK_ID = 1
+
+      before(async () => {
+        const result = [{ insertId: MOCK_ID }];
+        sinon.stub(connection, 'execute').resolves(result);
+      });
+      after(async () => {
+        connection.execute.restore();
+      });
+
+      it('returns an array', async () => {
+        const response = await productsModel.registerProduct(MOCK_NAME);
+        expect(response).to.be.an('array');
+      });
+      it('inside the array exist an object', async () => {
+        const response = await productsModel.registerProduct(MOCK_NAME);
+        expect(response[0]).to.be.an('object');
+      });
+      it('the object contain the correct ID', async () => {
+        const response = await productsModel.registerProduct(MOCK_NAME);
+        expect(response[0]).to.have.property('id');
+        expect(response[0].id).to.be.equal(MOCK_ID);
+      });
+      it("the object contain the product name", async () => {
+        const response = await productsModel.registerProduct(MOCK_NAME);
+        expect(response[0]).to.have.property('name');
+        expect(response[0].name).to.be.equal(MOCK_NAME);
+      });
+    });
+  });
+
+  describe('Verificação da função updateProduct', () => {
+    before(async () => {
+      const result = [[{ id: 1 }]];
+      sinon.stub(connection, 'execute').resolves(result);
+    });
+
+    after(async () => {
+      connection.execute.restore();
     })
+  });
+
+  describe('Verificação da função deleteProduct', () => {
+
   });
 });
