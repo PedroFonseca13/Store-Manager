dc53fa638c6badcc3b82f12e0854af3e0080b6a7
Pedro Fonseca <pedrinhopedro01@gmail.com>
2022-08-01 16:48:32 -0300

    teste: Maldito seja o back-end

--- tests/unit/models/productsModel.test.js
+++ tests/unit/models/productsModel.test.js
@@ -4,9 +4,9 @@ const sinon = require('sinon');
 const connection = require('../../../models/connection');
 const productsModel = require('../../../models/productsModel');
 
-describe('Testes de productsModel', () => {
+describe('Testes de productsModel.', () => {
 
-  describe('Verificação da função getAll', () => {
+  describe('Verificação da função getAll.', () => {
 
     before(async () => {
       const result = [[{ id: 1 }]];
@@ -16,20 +16,22 @@ describe('Testes de productsModel', () => {
     after(async () => {
       connection.execute.restore();
     })
+    describe('Se a requisição for bem sucedida.', () => {
 
-    it('returns an array', async () => {
-      const response = await productsModel.getAll();
-      expect(response).to.be.an('array');
-    });
+      it('Deve retorna um array.', async () => {
+        const response = await productsModel.getAll();
+        expect(response).to.be.an('array');
+      });
 
-    it('inside the array exists at least one object', async () => {
-      const response = await productsModel.getAll();
-      expect(response[0]).to.be.an('object');
-    });
+      it('Dentro do array, deve haver um objeto.', async () => {
+        const response = await productsModel.getAll();
+        expect(response[0]).to.be.an('object');
+      });
 
-    it('inside this object exists a property ID', async () => {
-      const response = await productsModel.getAll();
-      expect(response[0]).to.have.property('id');
+      it('Dentro do objeto, deve haver a propriedade "id".', async () => {
+        const response = await productsModel.getAll();
+        expect(response[0]).to.have.property('id');
+      });
     });
   });
 
@@ -44,19 +46,19 @@ describe('Testes de productsModel', () => {
       connection.execute.restore();
     });
 
-    describe('When the request is successfully completed', () => {
-      it('returns an array', async () => {
+    describe('Se a requisição for bem sucedida.', () => {
+      it('Deve retorna um array.', async () => {
         const response = await productsModel.findById(1);
         expect(response).to.be.an('array');
       });
 
-      it('inside the array exists one object', async () => {
+      it('Dentro do array, deve haver um objeto.', async () => {
         const response = await productsModel.findById(1);
         expect(response[0]).to.be.an('object');
         expect(response.length).to.be.equal(1);
       });
 
-      it('inside this object exists a property ID', async () => {
+      it('Dentro do objeto, deve haver a propriedade "id".', async () => {
         const response = await productsModel.findById(1);
         expect(response[0]).to.have.property('id');
       });
@@ -64,51 +66,99 @@ describe('Testes de productsModel', () => {
   });
 
   describe('Verificação da função registerProduct', () => {
-    describe('When the POST is successfully completed', () => {
-      const MOCK_NAME = 'Steve Rogers'
-      const MOCK_ID = 1
+    const MOCK_NAME = 'Steve Rogers'
+    const MOCK_ID = 1
 
-      before(async () => {
-        const result = [{ insertId: MOCK_ID }];
-        sinon.stub(connection, 'execute').resolves(result);
-      });
-      after(async () => {
-        connection.execute.restore();
-      });
+    before(async () => {
+      const result = [{ insertId: MOCK_ID }];
+      sinon.stub(connection, 'execute').resolves(result);
+    });
+    after(async () => {
+      connection.execute.restore();
+    });
 
-      it('returns an array', async () => {
+    describe('Se a requisição for bem sucedida.', () => {
+      it('Deve retornar um array', async () => {
         const response = await productsModel.registerProduct(MOCK_NAME);
         expect(response).to.be.an('array');
       });
-      it('inside the array exist an object', async () => {
+      it('Dentro do array, deve haver um objeto.', async () => {
         const response = await productsModel.registerProduct(MOCK_NAME);
         expect(response[0]).to.be.an('object');
       });
-      it('the object contain the correct ID', async () => {
+      it('Dentro do objeto, deve haver a propriedade "id".', async () => {
         const response = await productsModel.registerProduct(MOCK_NAME);
         expect(response[0]).to.have.property('id');
         expect(response[0].id).to.be.equal(MOCK_ID);
       });
-      it("the object contain the product name", async () => {
+      it('Dentro do objeto, deve haver a propriedade "name".', async () => {
         const response = await productsModel.registerProduct(MOCK_NAME);
         expect(response[0]).to.have.property('name');
         expect(response[0].name).to.be.equal(MOCK_NAME);
       });
-    });
+    })
   });
 
   describe('Verificação da função updateProduct', () => {
-    before(async () => {
-      const result = [[{ id: 1 }]];
-      sinon.stub(connection, 'execute').resolves(result);
-    });
+    const id = 1;
+    const name = 'skunk';
+    const quantity = 2;
 
-    after(async () => {
+    before(() => {
+      const execute = [{
+        affectedRows: 1,
+      }];
+
+      sinon.stub(connection, 'execute').resolves(execute);
+    })
+
+    after(() => {
       connection.execute.restore();
+    })
+
+    describe('Se a requisição for bem sucedida.', async () => {
+      it('Deve retorna um objeto', async () => {
+        const response = await productsModel.updateProduct(id, name, quantity);
+
+        expect(response).to.be.a('object');
+      });
+
+      it('Deve retornar um objeto com as propriedades "id", "name" e "quantity"', async () => {
+        const response = await productsModel.updateProduct(id, name, quantity);
+
+        expect(response).to.have.a.property('id');
+        expect(response).to.have.a.property('name');
+        expect(response).to.have.a.property('quantity');
+      });
     })
   });
 
   describe('Verificação da função deleteProduct', () => {
 
+    const id = 1;
+
+    before(() => {
+      const execute = [{
+        affectedRows: 1,
+      }];
+
+      sinon.stub(connection, 'execute').resolves(execute);
+    })
+
+    after(() => {
+      connection.execute.restore();
+    })
+
+    describe('Se a requisição for bem sucedida.', async () => {
+      it('retorna um objeto', async () => {
+        const response = await productsModel.deleteProduct(id);
+        expect(response).to.be.a('object');
+      });
+
+      it('com a propriedade "id"', async () => {
+        const response = await productsModel.deleteProduct(id);
+        expect(response).to.have.a.property('id');
+      });
+    })
   });
 });
